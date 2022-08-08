3a0429b7c783924e27ffbe215a12bc099e9d62df
Mathieu Bouthors <mathieubouthors@hotmail.com>
2022-08-08 20:36:24 +0200

    Responsive ok pour edit page og stash, speciality, person and for mission in progress

--- views/admin/stash/edit.php
+++ views/admin/stash/edit.php
@@ -147,7 +147,7 @@ if (!empty($params)) {
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="cancelSvg actionSvg" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                         </svg>
-                        Annuler
+                        <span class="textBtn">Annuler</span>
                     </a>
                 </button>
                 <button type="submit" class="confirmBtn actionBtn">
@@ -155,7 +155,7 @@ if (!empty($params)) {
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="confirmSvg actionSvg" viewBox="0 0 16 16">
                             <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
                         </svg>
-                        Valider
+                        <span class="textBtn">Valider</span>
                     </span>
                 </button>
             </div>
@@ -175,7 +175,7 @@ if (!empty($params)) {
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="deleteSvg actionSvg" viewBox="0 0 16 16">
                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                     </svg>
-                    Supprimer
+                    <span class="textBtn">Supprimer</span>
                 </span>
             </button>
         </form>
