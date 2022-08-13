decd73c56c6a0d470e894b9fd4cad584ee87165e
Mathieu Bouthors <mathieubouthors@hotmail.com>
2022-08-13 16:40:02 +0200

    Modify Connection for choice betwenn dev and prod

--- src/Connection.php
+++ src/Connection.php
@@ -1,13 +1,30 @@
 <?php
 
 namespace App;
-
+use Dotenv\Dotenv;
 use PDO;
 
 class Connection
 {
     public function getPdo(): PDO
     {
-        return new PDO('mysql:host=localhost;dbname=spy_agency;charset=utf8mb4', 'admin', 'admin');
+        
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+        $dotenv->load();
+
+        if ($_ENV['APP_ENV'] === 'prod') {
+            $url = parse_url($_ENV["CLEARDB_DATABASE_URL"]);
+        } elseif ($_ENV['APP_ENV'] === 'dev') {
+            $url = parse_url($_ENV["LOCAL_DATABASE_URL"]);
+        }
+        
+        $host = $url["host"];
+        $db_username = $url["user"];
+        $db_password = $url["pass"];
+        $db = substr($url["path"],1);
+        $active_group = 'default';
+        $query_builder = TRUE;
+
+        return new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $db_username, $db_password);
     }
 }
