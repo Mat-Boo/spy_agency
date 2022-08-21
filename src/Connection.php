aeb65ec1327b7a92cd82fb1eaa1baaf8c8658bc0
Mathieu Bouthors <mathieubouthors@hotmail.com>
2022-08-21 19:20:02 +0200

    Mdofiy env with JawsDb Infos

--- src/Connection.php
+++ src/Connection.php
@@ -13,18 +13,18 @@ class Connection
         $dotenv->load();
 
         if ($_ENV['APP_ENV'] === 'prod') {
-            $url = parse_url($_ENV["CLEARDB_DATABASE_URL"]);
+            $url = parse_url($_ENV["JAWSDB_DATABASE_URL"]);
         } elseif ($_ENV['APP_ENV'] === 'dev') {
             $url = parse_url($_ENV["LOCAL_DATABASE_URL"]);
         }
-        
+
         $host = $url["host"];
         $db_username = $url["user"];
         $db_password = $url["pass"];
         $db = substr($url["path"],1);
         $active_group = 'default';
         $query_builder = TRUE;
-
+        
         return new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $db_username, $db_password);
     }
 }
